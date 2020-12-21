23a7359dad14cf4a34e14e596f6fa6af5bc4cc75
Maksym <maksveremey4ik@gmail.com>
2020-12-21 17:09:40 +0200

    activ box

---
+++ active-box/js/main.js
@@ -0,0 +1,32 @@
+$(function () {
+    /*Fixed header*/
+    let header = $("#header");
+    let intro = $("#intro");
+    let introH = intro.innerHeight();
+    let scrollPos = $(window).scrollTop();
+
+    $(window).on("scroll load resize", function () {
+        introH = intro.innerHeight();
+        scrollPos = $(this).scrollTop();
+        
+        if( scrollPos > introH ) {
+            header.addClass("fixed");
+        } else {
+            header.removeClass("fixed");
+        }
+
+    });
+
+    /**/
+
+
+
+    $('.slider').slick({
+        slidesToShow: 1,
+        dots: false,
+        arrows: false,
+        slidesToScroll: 1,
+        autoplay: true,
+    });
+
+});
